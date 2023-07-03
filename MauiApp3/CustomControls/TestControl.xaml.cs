3f0865a96fdc871be66c456473dbca3581ed5b4b
Anders Laursen <al@miralix.com>
2023-07-03 15:29:05 +0200

    Add project files.

---
+++ MauiApp3/CustomControls/TestControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3.CustomControls;
+
+public partial class TestControl : ContentView
+{
+    public TestControl()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly BindableProperty TextProperty = BindableProperty.Create(
+        propertyName: nameof(Text),
+        returnType: typeof(string),
+        declaringType: typeof(TestControl),
+        defaultValue: null,
+        defaultBindingMode: BindingMode.TwoWay);
+
+
+    public string Text
+    {
+
+        get => (string)GetValue(TextProperty);
+        set
+        {
+            SetValue(TextProperty, value);
+        }
+    }
+}
